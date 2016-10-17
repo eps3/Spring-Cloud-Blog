c70a61292ed80e6c563ab632e897f622d3a51a3e
sheep3 <xuxin>
2016-10-17 15:35:34 +0800

    Fix: update config

--- client-post/src/main/java/cn/sheep3/cloud/client/post/Application.java
+++ client-post/src/main/java/cn/sheep3/cloud/client/post/Application.java
@@ -3,7 +3,9 @@ package cn.sheep3.cloud.client.post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
